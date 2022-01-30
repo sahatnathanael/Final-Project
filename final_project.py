910271f42365e6910a419612e7f4ef85531bba1b
sahatnathanael <97328940+sahatnathanael@users.noreply.github.com>
2022-01-30 15:17:52 +0700

    Add files via upload

---
+++ final_project.py
@@ -0,0 +1,67 @@
+import smtplib
+
+# SETUP EMAIL LOGIN 
+gmail_user = input(str("Masukkan akun gmail: "))
+gmail_app_password = input(str("Masukkan password gmail: "))
+
+# SETUP PENERIMA EMAIL
+with open('receiver_list.txt', 'r') as file:
+	penerima = file.read().replace('\n', ',')
+
+# SETUP Pengirim, penerima, judul dan isi email
+sent_from = gmail_user
+sent_to = input(str("Masukkan gmail penerima lalu akhiri dengan enter: "))
+sent_subject = input(str("Masukkan subjek atau judul: "))
+sent_body = input(str("Masukkan pesan yang akan dikirim : "))
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, ", ".join(sent_to), sent_subject, sent_body)
+
+# =============================================================================
+# Mengirim Email atau Gagal
+# Detail Sumber: http://www.samlogic.net/articles/smtp-commands-reference.htm
+# =============================================================================
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_app_password)
+    server.sendmail(sent_from, sent_to, email_text)
+    server.close()
+
+    print('Email sent!')
+except Exception as exception:
+    print("Error: %s!\n\n" % exception)
+
+#sumber : https://stackoverflow.com/questions/10147455/how-to-send-an-email-with-gmail-as-provider-using-python
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
